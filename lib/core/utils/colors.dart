683e99d80938efdbb00647d17cae3cb859eccf20
11 <k.abdalrhem@gmail.com>
2025-01-29 14:13:21 +0200

    1

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class Colors{
+  static Color black=Color(0xff2C2C2C);
+  static Color orange=Color(0xff67A41C);
+  static Color korange=Color(0xffEB5A3C);
+  static Color kblack=Color(0xff161616);
+  static Color kblackprimary=Color(0xff000000);
+  static Color kwhite=Color(0xffFFFFFF);
+  static Color KGreen=Color(0xff52B788);
+  static Color kBlue=Color(0xff2B59FE);
+  static Color Kpurple=Color(0xffD879D0);
+  static Color  kblackplay=Color(0xff582037);
+  static Color callfreind=Color(0xffd8dcf0cc);
+  static Color blackplus=Color(0xff000000);
+}
